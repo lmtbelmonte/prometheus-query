35ca3350182f6c5021ad9ff9f98e5c330547f261
luis merino <lumerino@redhat.com>
2023-06-25 10:03:30 +0200

    Accesso via libreria python prometheusconnect

---
+++ prometheus-api-client/mem-cpu-pod.py
@@ -0,0 +1,38 @@
+from openshift import client, config
+import pandas as pd
+
+# Connect to the OpenShift cluster
+config.load_kube_config()
+
+# Create an OpenShift API client
+v1 = client.CoreV1Api()
+
+# Get all pods in the cluster
+pods = v1.list_pod_for_all_namespaces(watch=False)
+
+# Initialize lists to store data
+data = []
+headers = ['Namespace', 'Pod', 'Container', 'CPU Request', 'CPU Limit', 'Memory Request', 'Memory Limit']
+
+# Iterate over the pods and extract the required information
+for pod in pods.items:
+    namespace = pod.metadata.namespace
+    pod_name = pod.metadata.name
+
+    for container in pod.spec.containers:
+        container_name = container.name
+        cpu_request = container.resources.requests.get('cpu', 'N/A')
+        cpu_limit = container.resources.limits.get('cpu', 'N/A')
+        mem_request = container.resources.requests.get('memory', 'N/A')
+        mem_limit = container.resources.limits.get('memory', 'N/A')
+
+        data.append([namespace, pod_name, container_name, cpu_request, cpu_limit, mem_request, mem_limit])
+
+# Create a pandas DataFrame from the extracted data
+df = pd.DataFrame(data, columns=headers)
+
+# Write the DataFrame to an Excel file
+output_file = 'pod_stats.xlsx'
+df.to_excel(output_file, index=False)
+print(f"Data exported to {output_file} successfully.")
+
---
+++ prometheus-api-client/mem-cpu-req-lim-pod-prometheus.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from prometheus_api_client import PrometheusConnect
+
+# Prometheus server URL
+prometheus_url = "http://<prometheus-server>:<port>"
+
+# Headers para poner el token OAUTH en la llamada
+auth_header = "Authorization": "bearer TOKEN"
+
+# Query to fetch the required metrics
+query = 'kube_pod_container_resource_requests_memory_bytes, kube_pod_container_resource_requests_cpu_cores, kube_pod_container_resource_limits_memory_bytes, kube_pod_container_resource_limits_cpu_cores, container_memory_usage_bytes, container_cpu_usage_seconds_total'
+
+# Connect to Prometheus - 
+# Autenticacion : Aqui podemos poner el header con el token  
+prometheus = PrometheusConnect(url=prometheus_url, headers=auth_header)
+
+# Get the metric data
+data = prometheus.custom_query(query)
+
+# Convert the data to a Pandas DataFrame
+df = pd.DataFrame(data)
+
+# Extract relevant information from the DataFrame
+df['month'] = pd.to_datetime(df['timestamp']).dt.strftime('%Y-%m')
+df['namespace'] = df['metric.namespace']
+df['pod'] = df['metric.pod']
+df['container'] = df['metric.container']
+df['memory_requests'] = df['value'][0]
+df['cpu_requests'] = df['value'][1]
+df['memory_limits'] = df['value'][2]
+df['cpu_limits'] = df['value'][3]
+df['memory_usage'] = df['value'][4]
+df['cpu_usage'] = df['value'][5]
+
+# Filter and reorganize the DataFrame
+df_filtered = df[['month', 'namespace', 'pod', 'container', 'memory_requests', 'cpu_requests', 'memory_limits', 'cpu_limits', 'memory_usage', 'cpu_usage']]
+
+# Save the data to an Excel file
+output_file = 'pod_metrics.xlsx'
+df_filtered.to_excel(output_file, index=False)
+print(f"Data saved to {output_file}")
+
